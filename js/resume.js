1cf4d839126a461045b8e41ae88ac8e8552a0ac3
Anano Topuria <anano.topuriaa@gmail.com>
2023-02-04 14:09:30 +0400

    pop-up

---
+++ js/resume.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//modal woindow
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(".close-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
