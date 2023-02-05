fedeb1220d93688db99908d3953a2d04c0a38995
Anano Topuria <anano.topuriaa@gmail.com>
2023-02-05 07:50:33 +0400

    info page error icons

--- js/script.js
+++ js/script.js
@@ -4,31 +4,78 @@
 // const surname = document.getElementById(`surname`);
 // const file = document.getElementById(`file`);
 // const aboutMe = document.getElementById(`about-me`);
-// const number = document.getElementById(`number`);
-// const email = document.getElementById(`email`);
 // const form = document.getElementById(`form`);
 
 // form.addEventListener(`submit`, (e = {
 //     e.preventDefault
 // }));
 
-const nameError = document.getElementById(`name-error`);
-const surnameError = document.getElementById(`surname-error`);
 const fileError = document.getElementById(`file-error`);
-const emailError = document.getElementById(`email-error`);
-const numberError = document.getElementById(`number-error`);
 
 function validateName() {
+  const nameError = document.getElementById(`name-error`);
   const name = document.getElementById(`name`).value;
-  if (name.length == 0) {
-    nameError.innerHTML = `Name is required`;
+  if (name.length < 2 || !/^[ა-ჰ]+$/.test(name)) {
+    nameError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
     return false;
   }
-  if (name.length < 2) {
-    nameError.innerHTML = `Name is required`;
-    return false;
-  }
-
-  nameError.innerHTML = `<ion-icon class="icon" name="checkmark-circle"></ion-icon>`;
+  nameError.innerHTML = `<ion-icon class="icon-check" name="checkmark-circle"></ion-icon>`;
   return true;
 }
+
+function validateSurname() {
+  const surnameError = document.getElementById(`surname-error`);
+  const surname = document.getElementById(`surname`).value;
+  if (surname.length < 2 || !/^[ა-ჰ]+$/.test(surname)) {
+    surnameError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
+    return false;
+  }
+  surnameError.innerHTML = `<ion-icon class="icon-check" name="checkmark-circle"></ion-icon>`;
+  return true;
+}
+
+function validateEmail() {
+  const emailError = document.getElementById(`email-error`);
+  const email = document.getElementById(`email`).value;
+  const emailRegex = /^[a-zA-Z0-9._-]+@redberry.ge$/;
+  if (!emailRegex.test(email)) {
+    emailError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
+    return false;
+  }
+  emailError.innerHTML = `<ion-icon class="icon-check" name="checkmark-circle"></ion-icon>`;
+  return true;
+}
+
+// function validatePhone() {
+//   const phoneError = document.getElementById(`phone-error`);
+//   const phone = document.getElementById(`number`).value;
+//   const phoneRegex = /^\\+995\\d{9}$/;
+//   if (!phoneRegex.test(phone)) {
+//     phoneError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
+//     return false;
+//   }
+
+//   phoneError.innerHTML = `<ion-icon class="icon-check" name="checkmark-circle"></ion-icon>`;
+//   return true;
+// }
+
+const phone = document.getElementById(`phone-number`).value;
+
+function validatePhone(phone) {
+  const phoneError = document.getElementById(`phone-error`);
+  // const number = parseFloat(phone);
+  console.log(`Phone: ${phone}`);
+  const phoneRegex = /^\+995\d{9}$/;
+  console.log(`Phone Regex: ${phoneRegex}`);
+  if (!phoneRegex.test(phone)) {
+    console.log(`Phone does not match pattern`);
+    phoneError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
+    return false;
+  }
+  console.log(`Phone matches pattern`);
+  phoneError.innerHTML = `<ion-icon class="icon-check" name="checkmark-circle"></ion-icon>`;
+  return true;
+}
+
+const dlkflsdfk = validatePhone(`+995123456789`);
+console.log(dlkflsdfk);
