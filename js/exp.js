69942ba68a408f80e268559cadae132e6780a318
Anano Topuria <anano.topuriaa@gmail.com>
2023-02-08 22:55:55 +0400

    dynamic experience fields

--- js/exp.js
+++ js/exp.js
@@ -4,9 +4,10 @@ function displayData(selector, value) {
   document.getElementById(selector).innerHTML = value;
 }
 
-function validatePosition() {
+function validatePosition(positionElement) {
   const positionError = document.getElementById(`position-error`);
-  const position = document.getElementById(`position`).value;
+  // const position = document.getElementById(`position`).value;
+  const position = positionElement.value;
   displayData(`a-position`, position);
   if (position.length < 2 || !/^[ა-ჰa-zA-Z\s]+$/.test(position)) {
     positionError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
@@ -16,9 +17,10 @@ function validatePosition() {
   return true;
 }
 
-function validateCompany() {
+function validateCompany(companyElement) {
   const companyError = document.getElementById(`company-error`);
-  const company = document.getElementById(`company`).value;
+  // const company = document.getElementById(`company`).value;
+  const company = companyElement.value;
   displayData(`a-company`, company);
   if (company.length < 2 || !/^[ა-ჰa-zA-Z\s]+$/.test(company)) {
     companyError.innerHTML = `<ion-icon class="icon-warning" name="warning"></ion-icon>`;
@@ -180,3 +182,106 @@ const redirectToPrevPage = function (selector, route) {
 };
 
 redirectToPrevPage(`.prev`, `./info.html`);
+var counter = 0;
+document.getElementById("addMoreExperience").addEventListener("click", () => {
+  counter++;
+  `a-position``position_1`;
+  console.log("add more rows");
+  let hr = document.createElement("hr");
+  let form = document.getElementById("experience-form");
+  let positionDiv = document.createElement("div");
+  form.appendChild(hr);
+  // Position
+  positionDiv.setAttribute("class", "position posit");
+  let positionLabel = document.createElement("label");
+  positionLabel.setAttribute("for", "position");
+  positionLabel.innerHTML = "თანამდებობა";
+  let positionInput = document.createElement("input");
+  positionInput.setAttribute("id", "position");
+  positionInput.setAttribute("name", "position[]");
+  positionInput.setAttribute("type", "text");
+  positionInput.setAttribute("placeholder", "დეველოპერი, დიზაინერი, ა.შ.");
+  positionInput.onkeyup = validatePosition(positionInput);
+
+  positionDiv.appendChild(positionLabel);
+  positionDiv.appendChild(positionInput);
+  form.appendChild(positionDiv);
+
+  //Company
+  let companyDiv = document.createElement("div");
+  companyDiv.setAttribute("class", "company posit");
+  let companyLabel = document.createElement("label");
+  companyLabel.setAttribute("for", "company");
+  companyLabel.innerHTML = "დამსაქმებელი";
+  let companyInput = document.createElement("input");
+  companyInput.setAttribute("id", "company");
+  companyInput.setAttribute("name", "company[]");
+  companyInput.setAttribute("type", "text");
+  companyInput.setAttribute("placeholder", "დამსაქმებელი");
+  companyInput.onkeyup = validatePosition(companyInput);
+
+  companyDiv.appendChild(companyLabel);
+  companyDiv.appendChild(companyInput);
+  form.appendChild(companyDiv);
+
+  //Date
+  let experienceDiv = document.createElement("div");
+  experienceDiv.setAttribute("class", "exp-date");
+  let experienceStart = document.createElement("div");
+  experienceStart.setAttribute("class", "st-date");
+  let startLabel = createLabel("start-date", "posit", "დაწყების რიცხვი");
+  let startDateInputElement = createInput(
+    "start-date",
+    "st[]",
+    "date",
+    "",
+    validateDateStart
+  );
+  var startDateError = createSpan("st-error");
+
+  experienceStart.appendChild(startLabel);
+  experienceStart.appendChild(startDateInputElement);
+  experienceStart.appendChild(startDateError);
+  experienceDiv.appendChild(experienceStart);
+  let experienceEnd = document.createElement("div");
+  experienceEnd.setAttribute("class", "en-date");
+  let endLabel = createLabel("end-date", "posit", "დამთავრების რიცხვი");
+  let endDateInputElement = createInput(
+    "end-date",
+    "en[]",
+    "date",
+    "",
+    validateDateEnd
+  );
+  var endDateError = createSpan("st-error");
+  experienceEnd.appendChild(endLabel);
+  experienceEnd.appendChild(endDateInputElement);
+  experienceEnd.appendChild(endDateError);
+  experienceDiv.appendChild(experienceEnd);
+
+  form.appendChild(experienceDiv);
+});
+
+function createLabel(classAttribute, forAttribute, value) {
+  let label = document.createElement("label");
+  label.setAttribute("class", classAttribute);
+  label.setAttribute("for", forAttribute);
+  label.innerHTML = value;
+  return label;
+}
+
+function createInput(id, name, type, placeholder, onChangeFunc) {
+  let dateStartInput = document.createElement("input");
+  dateStartInput.setAttribute("id", id);
+  dateStartInput.setAttribute("name", name);
+  dateStartInput.setAttribute("type", type);
+  dateStartInput.setAttribute("placeholder", placeholder);
+  dateStartInput.onchange = onChangeFunc(dateStartInput);
+  return dateStartInput;
+}
+
+function createSpan(id) {
+  let span = document.createElement("span");
+  span.setAttribute("id", "st-error");
+  return span;
+}
